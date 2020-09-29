96c0848b02e87292d9ca0e7c90b011a1817b48fc
evolvingkid <fayezmohammed23@gmail.com>
2020-09-29 13:39:36 +0530

    status code added

--- api/auth/emailandpassword.js
+++ api/auth/emailandpassword.js
@@ -2,14 +2,16 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const config = require('config');
-var admin = require("firebase-admin");
-const adminSdk = require('../../config/firebaseadminsdk');
 const bcrypt = require('bcrypt');
 const User = require('../../models/users');
 
 const serverAuthToken = config.get('server-auth-token');
 const saltRounds = config.get('password-salt-round');
 
+// * email and password user sigin
+
+
+// * email and password user creation
 router.post('/', [
     check('password', 'Password is required').not().isEmpty(),
     check('name', 'name is required').not().isEmpty(),
@@ -18,7 +20,7 @@ router.post('/', [
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.json({
+        return res.status(400).json({
             errors: errors.array()
         });
     }
@@ -26,42 +28,40 @@ router.post('/', [
     const token = req.header('server-auth-token');
 
     if (token == undefined) {
-        return res.json({
+        return res.status(401).json({
             errors: 'You dont have permission to acess this servers'
         });
     }
 
     if (token != serverAuthToken) {
-        return res.json({
+        return res.status(401).json({
             errors: 'You dont have permission to acess this servers'
         });
     }
+    try {
+        let { email, password, name } = req.body;
 
-    let { email, password, name } = req.body;
+        let hashPassword = await bcrypt.hash(password, saltRounds);
 
-    let hashPassword = await bcrypt.hash(password, saltRounds);
+        user = new User({
+            name: name,
+            password: hashPassword,
+            email: email,
+        });
 
-    user = new User({
-        name: name,
-        password: hashPassword,
-        email: email,
-        address: {
-            country: null,
-            administartion: null,
-            subadministration: null,
-            postalcode: null,
-            place: null,
-        },
-        order: null
-    });
+        await user.save();
 
-    await user.save();
+        return res.status(201).json({
+            sucess: true,
+            data: user
+        });
 
-    return res.json({
-        sucess: true,
-        data: user
-    });
+    } catch (error) {
+        return res.status(500).json({
+            errors: 'Error occured',
+        });
 
+    }
 });
 
 module.exports = router;
--- models/users.js
+++ models/users.js
@@ -4,14 +4,7 @@ const UsersModel = new mongoose.Schema({
     name: { type: String },
     password: { type: String },
     email: { type: String },
-    address: {
-        country: { type: String },
-        administartion: { type: String },
-        subadministration: { type: String },
-        postalcode: { type: Number },
-        place: { type: String },
-    },
-    order: {},
+    isEmailVerified: { type: Boolean },
 });
 
 module.exports = User = mongoose.model('UsersModel', UsersModel);
